40c0a8b437549223749642f288428800315dd0df
patricia bagzai <patricia5859.ccs.neu.edu>
2016-04-07 14:05:56 -0400

    fixing goole places

--- public/server/services/places.service.server.js
+++ public/server/services/places.service.server.js
@@ -10,7 +10,8 @@ module.exports = function(app){
     function searchPlaces(req, res){
 
         var searchQuery = req.query.search;
-        var url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670,151.1957&radius=500&name='+searchQuery+'&key=AIzaSyDqu1FqTmzBY0ogfnDSOkKK2Q9VmsnI7Eo='
+        var location;
+        var url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query='+searchQuery+'&location='+location+'&key=AIzaSyDqu1FqTmzBY0ogfnDSOkKK2Q9VmsnI7Eo='
         request(url, function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 res.send(body);
